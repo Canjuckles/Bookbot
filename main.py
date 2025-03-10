081edc7b0e9669eb58425f7ffae28da1fc868121
Canjuckles <julianirassar@gmail.com>
2025-03-10 15:51:41 -0400

    Divided stats into a seperate doc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def main():
-    print(count_words(get_book_text("books/frankenstein.txt")))
-
+    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 
 
 def get_book_text(filepath): 
